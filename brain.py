c0b2e88087f8c865117024d4b6aee50ba51c3000
hu.po <hugo.p.cmu@gmail.com>
2023-10-30 14:07:53 -0500

    progress

--- brain.py
+++ brain.py
@@ -4,37 +4,25 @@ import sys
 import subprocess
 import time
 import base64
-from io import BytesIO
 
-from .utils import scrape_callback, send_file
-from .hparams import HPARAMS
+# from .utils import scrape_callback, send_file
+from hparams import HPARAMS
 import requests
-from PIL import Image
-from dataclasses import dataclass
+# from PIL import Image
 
-@dataclass
 class MiniDocker:
 
-    name: str
-    port: str
-    proc: subprocess.Popen
-    warmup: int = 30
-
-    def __post_init__(self):
-        # Run the controlnet docker container and remove it after use
+    def __init__(self, name: str = 'llava13b', port: str = '5000', warmup: int = 30):
         self.nuke()
-        self.proc = subprocess.Popen(
-            [
-                "docker",
-                "run",
-                "--rm",
-                "-p",
-                "5000:5000",
-                "--gpus=all",
-                "controlnet_container",
-            ]
-        )
-        time.sleep(self.warmup)  # Let the docker container startup
+        self.name, self.port, self.warmup = name, port, warmup
+        self.proc = subprocess.Popen([
+            "docker", "run", "--rm", 
+            "-v", "/home/oop/dev/LLaVA/llava-v1.5-13b:/src/liuhaotian/llava-v1.5-13b", 
+            "-p", f"{port}:{port}", 
+            "--gpus=all", 
+            name
+        ])
+        time.sleep(self.warmup)
 
     def nuke(self):
         containers = os.popen("docker ps -aq").read().strip()
@@ -63,6 +51,10 @@ async def run_vlm(
 # https://replicate.com/yorickvp/llava-13b
 docker run --name llava13b r8.im/yorickvp/llava-13b@sha256:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591
 docker commit llava13b llava13b
+# Use weights stored locally (also needs to download clip336)
+docker run -v /home/oop/dev/LLaVA/llava-v1.5-13b:/src/liuhaotian/llava-v1.5-13b --gpus=all llava13b
+docker commit llava13b llava13b
+
     """
     image_path = os.path.join(brain_data_dir, input_image_path)
     with open(image_path, "rb") as img_file:
@@ -73,7 +65,6 @@ docker commit llava13b llava13b
                 "input": {
                     "image": f"data:image/png;base64,{base64.b64encode(img_file.read()).decode('utf-8')}",
                     "prompt": prompt,
-                    "seed": 42,
                 },
             },
         )
@@ -83,32 +74,37 @@ docker commit llava13b llava13b
 
 async def main_loop(hparams: dict = HPARAMS):
 
-    # Generate a unique id for this generation session
-    session_id = str(uuid.uuid4())[:6]
+    # # Generate a unique id for this generation session
+    # session_id = str(uuid.uuid4())[:6]
 
-    # Create a output folder for the session id and use that as the output dir
-    output_dir = os.path.join(brain_data_dir, session_id)
-    os.makedirs(output_dir, exist_ok=True)
+    # # Create a output folder for the session id and use that as the output dir
+    # output_dir = os.path.join(brain_data_dir, session_id)
+    # os.makedirs(output_dir, exist_ok=True)
 
 
     # scrape for image + image log
 
     # scrape for servo + servo log
 
-    results = await asyncio.gather(*_tasks, return_exceptions=True)
+    # results = await asyncio.gather(*_tasks, return_exceptions=True)
 
-    # ask VLM for commands
+    # # ask VLM for commands
 
-    results = await asyncio.gather(*_tasks, return_exceptions=True)
+    # results = await asyncio.gather(*_tasks, return_exceptions=True)
     
-    # write commands, send commands
+    # # write commands, send commands
 
-    results = await asyncio.gather(*_tasks, return_exceptions=True)
+    # results = await asyncio.gather(*_tasks, return_exceptions=True)
+
+    pass
 
 async def test(hparams: dict = HPARAMS):
+
     pass
 
 
 if __name__ == "__main__":
-    asyncio.run(test())
-    asyncio.run(main_loop())
+    docker = MiniDocker()
+    asyncio.run(main_loop(docker))
+    del docker
+    print("done")
