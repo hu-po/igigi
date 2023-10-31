cbd5ff7c2e7e5a44ef0a39fdd590a1691d4ecdeb
hu.po <hugo.p.cmu@gmail.com>
2023-10-31 09:51:02 -0500

    copypaste

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 import subprocess
 import sys
 import logging
+import time
 
 import gradio as gr
+from hparams import HPARAMS
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -19,28 +21,40 @@ def remote_chromium_gradio_ui(
     return subprocess.Popen(_cmd, stdin=subprocess.PIPE)
 
 
-def update_interface(servo1, servo2, servo3):
-    return None
+def display_video_image_text(video_path, image_path, text_path):
+    """Displays a video, image, and text file from local paths."""
 
+    # Load the video, image, and text file.
+    with open(video_path, "rb") as f:
+        video_bytes = f.read()
 
-# Sliders for each servo
-servo1_slider = gr.Slider(minimum=0, maximum=360, value=180, label="Servo 1")
-servo2_slider = gr.Slider(minimum=0, maximum=360, value=180, label="Servo 2")
-servo3_slider = gr.Slider(minimum=0, maximum=360, value=180, label="Servo 3")
-image = gr.Image(shape=(480, 640), image_mode="L", invert_colors=True, label="Camera")
+    with open(image_path, "rb") as f:
+        image_bytes = f.read()
 
-# Creating the interface
-iface = gr.Interface(
-    fn=update_interface,
-    inputs=[servo1_slider, servo2_slider, servo3_slider],
-    outputs=[],
-    live=True,
-)
+    with open(text_path, "r") as f:
+        text = f.read()
+
+    # Create the Gradio Interface.
+    interface = gr.Interface(fn=display_video_image_text, inputs=[], outputs=["video", "image", "text"])
+
+    # Set the initial values of the output components.
+    interface.outputs["video"].update(video_bytes)
+    interface.outputs["image"].update(image_bytes)
+    interface.outputs["text"].update(text)
+
+    # Start a loop to refresh the output components every 1 second.
+    while True:
+        interface.refresh()
+        time.sleep(1)
 
 if __name__ == "__main__":
     try:
         process = remote_chromium_gradio_ui()
-        iface.launch()
+        display_video_image_text(
+            os.path.join(HPARAMS.get("robot_data_dir"), HPARAMS.get("video_filename")),
+            os.path.join(HPARAMS.get("robot_data_dir"), HPARAMS.get("image_filename")),
+            os.path.join(HPARAMS.get("robot_data_dir"), HPARAMS.get("robotlog_filename")),
+        )
     except KeyboardInterrupt:
         print("Remote chromium browser terminated.")
         sys.exit(0)
