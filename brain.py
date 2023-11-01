00675722b937c84a39af3ed1a0262135db6e419f
hu.po <hugo.p.cmu@gmail.com>
2023-11-01 10:21:51 -0500

    123123

--- brain.py
+++ brain.py
@@ -1,65 +1,100 @@
 import asyncio
 import os
+from typing import Any, Dict
 
 from hparams import HPARAMS
-from utils import find_file, send_file, create_session_folder
+from utils import find_file, send_file, create_session_folder, async_timeout
 from vlm import VLMDocker, run_vlm
 
 
-async def main_loop(hparams: dict = HPARAMS):
-    """
-    the following this is a set of asynchronous functions
-    some functions require the output of other functions
-    based on previous history you must select one of these functions
-    the functions are
-
-    the history is
-    """
+@async_timeout(timeout=HPARAMS["timeout_brain_main_loop"])
+async def main_loop() -> Dict[str, Any]:
+    print("Starting main loop.")
+    log: str = ""
     # Batch 1: get robot log, get mono image
     task_batch = [
         find_file(
             HPARAMS["robotlog_filename"],
             os.path.join(HPARAMS["brain_data_dir"], HPARAMS["session_name"]),
-            HPARAMS["scrape_interval"],
-            HPARAMS["scrape_timeout"],
+            HPARAMS["find_file_interval"],
         ),
         find_file(
             HPARAMS["image_filename"],
             os.path.join(HPARAMS["brain_data_dir"], HPARAMS["session_name"]),
-            HPARAMS["scrape_interval"],
-            HPARAMS["scrape_timeout"],
+            HPARAMS["find_file_interval"],
         ),
     ]
     results = await asyncio.gather(*task_batch, return_exceptions=True)
-
-    # Batch 2: run vlm
-    task_batch = [
-        run_vlm(
-            HPARAMS["vlm_prompt"],
-            HPARAMS["vlm_docker_url"],
+    for result in results:
+        log += result["log"]
+    task_batch = []
+    # Executing VLM requires an image
+    if results[0].get("full_path", None) is None:
+        log += "No image found."
+    elif results[0]["file_age"] > HPARAMS["image_max_age"]:
+        log += "Image is too old."
+    else:
+        log += "Adding run_vlm to tasks."
+        task_batch.append(
+            run_vlm(
+                HPARAMS["vlm_prompt"],
+                HPARAMS["vlm_docker_url"],
+                os.path.join(
+                    HPARAMS["brain_data_dir"],
+                    HPARAMS["session_name"],
+                    HPARAMS["image_filename"],
+                ),
+            )
+        )
+    if len(task_batch) == 0:
+        return {"log": log}
+    results = await asyncio.gather(*task_batch, return_exceptions=True)
+    for result in results:
+        log += result["log"]
+    task_batch = []
+    commands = results[0].get("reply", None)
+    print(f"Commands: {commands}")
+    if commands is None:
+        log += "No commands from VLM."
+    else:
+        with open(
             os.path.join(
                 HPARAMS["brain_data_dir"],
                 HPARAMS["session_name"],
-                HPARAMS["image_filename"],
+                HPARAMS["commands_filename"],
             ),
-        ),
-    ]
+            "w",
+        ) as f:
+            f.write(commands)
+        log += "Adding send_file to tasks."
+        task_batch.append(
+            send_file(
+                HPARAMS["commands_filename"],
+                os.path.join(HPARAMS["brain_data_dir"], HPARAMS["session_name"]),
+                os.path.join(HPARAMS["robot_data_dir"], HPARAMS["session_name"]),
+                HPARAMS["robot_username"],
+                HPARAMS["robot_ip"],
+            ),
+        )
+    if len(task_batch) == 0:
+        return {"log": log}
     results = await asyncio.gather(*task_batch, return_exceptions=True)
-
-    # Batch 3: send commands
-    task_batch = [
-        send_file(
-            HPARAMS["commands_filename"],
-            os.path.join(HPARAMS["brain_data_dir"], HPARAMS["session_name"]),
-            os.path.join(HPARAMS["robot_data_dir"], HPARAMS["session_name"]),
-            HPARAMS["robot_username"],
-            HPARAMS["robot_ip"],
-        ),
-    ]
-    results = await asyncio.gather(*task_batch, return_exceptions=True)
+    for result in results:
+        log += result["log"]
+    return {"log": log}
 
 
 if __name__ == "__main__":
     HPARAMS["session_name"] = create_session_folder(HPARAMS["brain_data_dir"])
     docker = VLMDocker()
-    asyncio.run(main_loop())
+    while True:
+        result = asyncio.run(main_loop())
+        print(result["log"])
+        output_path = os.path.join(
+            HPARAMS["brain_data_dir"],
+            HPARAMS["session_name"],
+            HPARAMS["brainlog_filename"],
+        )
+        with open(output_path, "w") as f:
+            f.write(result["log"])
+
--- robot.py
+++ robot.py
@@ -1,16 +1,19 @@
 import asyncio
 import os
+from typing import Any, Dict
 
 from hparams import HPARAMS
-from utils import find_file, send_file, create_session_folder
+from utils import find_file, send_file, create_session_folder, async_timeout
 from record import take_image, record_video, CAMERAS
 from llm import move_servos
 from servos import Servos
 from app import ChromeUI
 
 
-async def main_loop(servos: Servos, ui: ChromeUI):
-    robot_log: str = ""
+@async_timeout(timeout=HPARAMS["timeout_robot_main_loop"])
+async def main_loop(servos: Servos, ui: ChromeUI) -> Dict[str, Any]:
+    print("Starting main loop.")
+    log: str = ""
     # Batch 1: get commands, take mono image
     task_batch = [
         find_file(
@@ -25,16 +28,16 @@ async def main_loop(servos: Servos, ui: ChromeUI):
         ),
     ]
     results = await asyncio.gather(*task_batch, return_exceptions=True)
-    # Create new task batch
+    for result in results:
+        log += result["log"]
     task_batch = []
     # Moving servos requires a commands file
-    robot_log += results[0]["log"]
     if results[0].get("full_path", None) is None:
-        robot_log += "No command file found."
+        log += "No command file found."
     elif results[0]["file_age"] > HPARAMS["commands_max_age"]:
-        robot_log += "Command file is too old."
+        log += "Command file is too old."
     else:
-        robot_log += "Adding move_servos to tasks."
+        log += "Adding move_servos to tasks."
         with open(results[0]["full_path"], "r") as f:
             commands = f.read()
         print(f"Commands: {commands}")
@@ -49,9 +52,8 @@ async def main_loop(servos: Servos, ui: ChromeUI):
             ),
         )
     # Sending image requires an error free image capture
-    robot_log += results[1]["log"]
     if results[1].get("output_path") is not None:
-        robot_log += "Adding send_file to tasks."
+        log += "Adding send_file to tasks."
         task_batch.append(
             send_file(
                 HPARAMS["image_filename"],
@@ -62,7 +64,7 @@ async def main_loop(servos: Servos, ui: ChromeUI):
             ),
         )
     # Batch 2: send mono image, move servos, record stereo video
-    robot_log += "Adding record_video to tasks."
+    log += "Adding record_video to tasks."
     task_batch.append(
         record_video(
             CAMERAS["stereo"],
@@ -73,12 +75,12 @@ async def main_loop(servos: Servos, ui: ChromeUI):
         ),
     )
     results = await asyncio.gather(*task_batch, return_exceptions=True)
-    # Create new task batch
+    for result in results:
+        log += result["log"]
     task_batch = []
     # Sending video requires an error free capture
-    robot_log += results[-1]["log"]
     if results[-1].get("output_path") is not None:
-        robot_log += "Adding send_file to tasks."
+        log += "Adding send_file to tasks."
         task_batch.append(
             send_file(
                 HPARAMS["video_filename"],
@@ -88,13 +90,6 @@ async def main_loop(servos: Servos, ui: ChromeUI):
                 HPARAMS["brain_ip"],
             ),
         )
-    # Add any other results to robot log
-    for result in results[:-1]:
-        robot_log += result["log"]
-    # Write robot log to file
-    output_path = os.path.join(HPARAMS["robot_data_dir"], HPARAMS["session_name"], HPARAMS["robotlog_filename"])
-    with open(output_path, "w") as f:
-        f.write(robot_log)
     # Batch 3: send robot log, send stereo video, update ui
     task_batch.append(
         send_file(
@@ -109,6 +104,9 @@ async def main_loop(servos: Servos, ui: ChromeUI):
         ui.update_interface(),
     )
     results = await asyncio.gather(*task_batch, return_exceptions=True)
+    for result in results:
+        log += result["log"]
+    return {"log": log}
 
 
 if __name__ == "__main__":
@@ -123,4 +121,12 @@ if __name__ == "__main__":
         HPARAMS.get("robotlog_filename"),
     )
     while True:
-        asyncio.run(main_loop(servos, ui))
+        result = asyncio.run(main_loop(servos, ui))
+        print(result["log"])
+        output_path = os.path.join(
+            HPARAMS["robot_data_dir"],
+            HPARAMS["session_name"],
+            HPARAMS["robotlog_filename"],
+        )
+        with open(output_path, "w") as f:
+            f.write(result["log"])
